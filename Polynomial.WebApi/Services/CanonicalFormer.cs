2fa9cacd56890b1b529fc399a27186ab606ac229
Evgenii Zakharov <eugene.zakharov93@gmail.com>
2020-03-30 10:11:33 +0300

    add validation via Regexp, remove unused PolynomialErrorStrategy

--- Polynomial.WebApi/Services/CanonicalFormer.cs
+++ Polynomial.WebApi/Services/CanonicalFormer.cs
@@ -1,4 +1,5 @@
-﻿using Antlr4.Runtime;
+﻿using System.Text.RegularExpressions;
+using Antlr4.Runtime;
 using Antlr4.Runtime.Misc;
 using Antlr4.Runtime.Tree;
 using Polynomial.WebApi.Domain.Entities;
@@ -7,14 +8,28 @@ namespace Polynomial.WebApi.Services
 {
     public class CanonicalFormer
     {
+        private static readonly Regex AllowedCharactersRegex;
+
+        static CanonicalFormer()
+        {
+            AllowedCharactersRegex = new Regex("^[0-9A-Za-z+-.=^()]*$", RegexOptions.Compiled | RegexOptions.IgnoreCase);
+        }
+
         public (string canonical, string errorMessage) ToCanonical(string expression)
         {
-            if (IsEmptyExpression(expression))
+            var (success, trimmedExpression) = TrimExpressionIfNotEmpty(expression);
+            if (!success)
             {
-                return (string.Empty, $"{nameof(expression)} is empty");
+                return (string.Empty, trimmedExpression);
             }
 
-            (IParseTree tree, string parseErrorMessage) = TryParseExpression(expression);
+            var (anyUnrecognizedCharacter, unrecognizedCharacterErrorMessage) = AnyUnrecognizedCharacter(trimmedExpression);
+            if (anyUnrecognizedCharacter)
+            {
+                return (string.Empty, unrecognizedCharacterErrorMessage);
+            }
+
+            (IParseTree tree, string parseErrorMessage) = TryParseExpression(trimmedExpression);
             if (string.IsNullOrEmpty(parseErrorMessage))
             {
                 PolynomialVisitor polynomialVisitor = new PolynomialVisitor();
@@ -26,20 +41,38 @@ namespace Polynomial.WebApi.Services
             return (string.Empty, parseErrorMessage);
         }
 
-        private static bool IsEmptyExpression(string expression)
+        private static (bool success, string trimmedResult) TrimExpressionIfNotEmpty(string expression)
         {
             if (string.IsNullOrEmpty(expression))
             {
-                return true;
+                return (false, $"{nameof(expression)} is empty");
             }
 
             string trimmedExpression = expression.Replace(" ", string.Empty);
             if (string.IsNullOrEmpty(trimmedExpression))
             {
-                return true;
+                return (false, $"{nameof(expression)} is empty");
             }
 
-            return false;
+            return (true, trimmedExpression);
+        }
+
+        /// <summary>
+        /// A hack with Regexp that checks expression on unrecognized characters.
+        /// It should be ANTLR parser responsibility, however it's not working, no clue why.
+        /// Raised SO question.
+        /// https://stackoverflow.com/questions/60920641/c-sharp-antlr4-defaulterrorstrategy-or-custom-error-listener-does-not-catch-unre
+        /// </summary>
+        /// <param name="expression"></param>
+        /// <returns></returns>
+        private static (bool isValid, string errorMessage) AnyUnrecognizedCharacter(string expression)
+        {
+            if (AllowedCharactersRegex.IsMatch(expression))
+            {
+                return (false, string.Empty);
+            }
+
+            return (true, "Expression contains unrecognized characters");
         }
 
         private static (IParseTree tree, string parseErrorMessage) TryParseExpression(string expression)
@@ -48,16 +81,10 @@ namespace Polynomial.WebApi.Services
             ITokenSource lexer = new PolynomialLexer(stream);
 
             ITokenStream tokens = new CommonTokenStream(lexer);
-            PolynomialParser parser = new PolynomialParser(tokens);
-            parser.ErrorHandler = new BailErrorStrategy();
-            
-            //TODO : Add a hack with validation -> use REGEXP and wait for an answer to SO ticket
-            //https://stackoverflow.com/questions/60920641/c-sharp-antlr4-defaulterrorstrategy-or-custom-error-listener-does-not-catch-unre
-            //And cover 2 failed unit tests
+            PolynomialParser parser = new PolynomialParser(tokens) {BuildParseTree = true};
 
-            //parser.RemoveErrorListeners();
-            //parser.AddErrorListener(new PolynomialErrorListener());
-            parser.BuildParseTree = true;
+            parser.RemoveErrorListeners();
+            parser.AddErrorListener(new PolynomialErrorListener());
 
             try
             {
