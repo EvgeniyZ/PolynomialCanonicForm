8d976779a5c56ce15d6e43caf1f3e53a516748e3
Evgenii Zakharov <eugene.zakharov93@gmail.com>
2020-03-31 10:21:10 +0300

    more userfriendly error messages

--- Polynomial.WebApi/Services/ErrorListeners/PolynomialErrorListener.cs
+++ Polynomial.WebApi/Services/ErrorListeners/PolynomialErrorListener.cs
@@ -1,4 +1,5 @@
 ﻿using System.IO;
+using System.Linq;
 using Antlr4.Runtime;
 using Antlr4.Runtime.Misc;
 
@@ -9,27 +10,35 @@ namespace Polynomial.WebApi.Services.ErrorListeners
         public override void SyntaxError(TextWriter output, IRecognizer recognizer, IToken offendingSymbol, int line, int charPositionInLine, string msg,
             RecognitionException e)
         {
+            var bufferedTokenStream = recognizer.InputStream as BufferedTokenStream;
             if (e is InputMismatchException)
             {
                 if (offendingSymbol.Text == "<EOF>")
                 {
-                    throw new ParseCanceledException($"{GetSyntaxErrorHeader(charPositionInLine)}. Missing an expression after '=' sign");
+                    IToken previousToken = GetPreviousToken(offendingSymbol, bufferedTokenStream);
+                    throw new ParseCanceledException($"{GetSyntaxErrorHeader(charPositionInLine)}. Missing an expression after '{previousToken?.Text}' sign");
                 }
 
-                throw new ParseCanceledException($"{GetSyntaxErrorHeader(++charPositionInLine)}. Missing an expression after {offendingSymbol.Text} sign");
+                throw new ParseCanceledException($"{GetSyntaxErrorHeader(++charPositionInLine)}. Missing an expression after '{offendingSymbol.Text}' sign");
             }
 
             if (e is NoViableAltException)
             {
-                throw new ParseCanceledException($"{GetSyntaxErrorHeader(charPositionInLine)}. Not closed operator");
+                IToken previousToken = GetPreviousToken(offendingSymbol, bufferedTokenStream);
+                throw new ParseCanceledException($"{GetSyntaxErrorHeader(charPositionInLine)}. Not closed '{previousToken?.Text}' operator");
             }
 
             if (msg.StartsWith("extraneous input"))
             {
-                throw new ParseCanceledException($"{GetSyntaxErrorHeader(++charPositionInLine)}. Unrecognized character {offendingSymbol.Text}");
+                throw new ParseCanceledException($"{GetSyntaxErrorHeader(++charPositionInLine)}. Unrecognized character '{offendingSymbol.Text}'");
             }
 
             throw new ParseCanceledException($"{GetSyntaxErrorHeader(charPositionInLine)}. {msg}");
+        }
+
+        private static IToken GetPreviousToken(IToken offendingSymbol, BufferedTokenStream bufferedTokenStream)
+        {
+            return bufferedTokenStream?.GetTokens(offendingSymbol.TokenIndex - 1, offendingSymbol.TokenIndex).First();
         }
 
         private static string GetSyntaxErrorHeader(int errorPosition)
